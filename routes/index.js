3f70745fda5bc04bbe0057fab31982de42f98d72
Đinh Ngọc Liên Nhi <dinhngocliennhi1999@gmail.com>
2022-11-13 20:52:52 +0700

    completted

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.status(200).send("Welcome to CoderSchool! ")
+});
+
+module.exports = router;
+
+const bookRouter= require('./book.api.js')
+router.use('/books',bookRouter)
